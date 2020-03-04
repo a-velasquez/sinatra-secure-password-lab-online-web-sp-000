b0b365350fc97f42b051db8c0cf9a57063455422
Alex Velasquez <jtinnesco@gmail.com>
2020-03-04 19:43:11 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,7 +18,8 @@ class ApplicationController < Sinatra::Base
 
   post "/signup" do
     #your code here
-
+    if params[:username].empty?
+      redirect to '/failure'
   end
 
   get '/account' do
