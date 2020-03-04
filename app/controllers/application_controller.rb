5e3a8137daee01757128a6e0afad0ce8f25a662a
Alex Velasquez <jtinnesco@gmail.com>
2020-03-04 19:52:05 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -41,16 +41,15 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/login" do
-    ##your code here
-    user = User.find_by(:username => params[:username])
+  user = User.find_by(:username => params[:username])
 
-    if user && user.authenicate(params[:password])
-      session[:user_id] = user.id
-      redirect to '/account'
-    else
-      redirect to '/failure'
-    end
+  if user && user.authenticate(params[:password])
+    session[:user_id] = user.id
+    redirect to '/account'
+  else
+    redirect to '/failure'
   end
+end
 
   get "/failure" do
     erb :failure
